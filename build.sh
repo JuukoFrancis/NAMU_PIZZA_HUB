7a7794ef62b3d428c32fb8cdd1a825d5295bbe04
JuukoFrancis <juukojuniorfrancis@gmail.com>
2025-05-14 11:00:00 +0300

    initial-commit

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
-set -o errexit
+# set -o errexit
 # pip install requirements.txt
+
+# python manage.py collectstatic --no-input
+# python manage.py migrate
+
+
+#!/bin/bash
+set -o errexit
+
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
