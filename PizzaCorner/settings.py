503ec42e3af21643c00f22575545669ca100ab3c
JuukoFrancis <juukojuniorfrancis@gmail.com>
2025-05-14 10:44:03 +0300

    initial-commit

--- PizzaCorner/settings.py
+++ PizzaCorner/settings.py
@@ -130,7 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 # Media
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
